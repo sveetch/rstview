3a253ac7c21d234f7e08b47d2c2fcca4307f9025
David THENON <sveetch@gmail.com>
2016-07-18 00:06:32 +0200

    Just re ordered tests

--- project_test/project/urls.py
+++ project_test/project/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
 
+    # Dummy homepage just for simple ping view
     url(r'^$', TemplateView.as_view(template_name="homepage.html"), name='home'),
 ]
